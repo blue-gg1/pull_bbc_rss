42db2242f89b352e758f03a9d2dc1c8d09ddc0cc
dev-173 <63912836+dev-173@users.noreply.github.com>
2020-05-02 20:57:55 +0300

    Update main_rss_feed.py

--- main_rss_feed.py
+++ main_rss_feed.py
@@ -1,16 +1,23 @@
 import requests
 import sys
 import re
-import wget
 from datetime import datetime
 import os
+
+now = datetime.now()
+dt_string = now.strftime("%d-%m-%Y_%H")
+print(dt_string)
+
+
 
 feedurl = "https://podcast.voice.api.bbci.co.uk/rss/audio/p002vsmz?api_key=Wbek5zSqxz0Hk1blo5IBqbd9SCWIfNbT"
 bbccontent = requests.get(feedurl)
 locationmp3 = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\), ]|(?:%[0-9a-fA-F][0-9a-fA-F]))+' ,bbccontent.text)[6]
 print(locationmp3)
 
-# now = datetime.now()
-# dt_string = now.strftime("%d-%m-%Y_%H-%M-%S")
-# filename = dt_string+'.mp3'
-# newname = os.path.join('/Users/bluepill/Documents/proj/rss/' , filename)
+empethree = requests.get(locationmp3)
+print("done downloading")
+
+
+with open('/proj/rss/mp3/'+dt_string+'.mp3','wb') as f:
+    f.write(empethree.content) 
